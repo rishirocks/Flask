cab1a88accaaa1ad9db3427aa6a5c4d0ab089502
rishirocks <127014244+rishirocks@users.noreply.github.com>
2023-03-12 06:23:28 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,22 +1,71 @@
-from flask import Flask,render_template,request
-
+import pandas as pd
+import os
+from flask import Flask,render_template,request,send_file,make_response
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl import Workbook,load_workbook
+from io import BytesIO
+from pathlib import Path
 
 app = Flask(__name__)
-
+wb = load_workbook('Marks.xlsx')
+sheet = wb.active
+row = sheet.max_row+1
 @app.route("/",methods=["POST","GET"])
-def calculate():
-       ones = ''
-       twos = ''
-       As   =' '
-       if request.method == "POST":
 
-         IA_1  = float(request.form.get('one'))
-         ones  = (IA_1*30)/100
-         IA_2  = float(request.form.get('two'))
-         twos  = (IA_2*30)/100
-         ASS_1 = float(request.form.get('ass'))
-         As    = ASS_1
-       return render_template("home.html",ones=ones,twos=twos,As=As)
+def submit_marks():
+    global row
+
+    IA_one = ''
+    IA_two = ''
+    Reg_no = ''
+    Assign = ''
+    if request.method == "POST":
+       if request.form.get('Reg_no'):
+         Reg_no = request.form.get('Reg_no')
+       else:
+         Reg_no = None
+
+       if request.form.get('IA_1'):
+         IA_1 = int(request.form.get('IA_1'))
+         IA_one = float((IA_1 * 30) / 100)
+       else:
+         IA_one = None
+
+       if request.form.get('IA_2'):
+         IA_2 = int(request.form.get('IA_2'))
+         IA_two = float((IA_2 * 30) / 100)
+       else:
+         IA_two =None
+
+       if request.form.get('Assign'):
+         Assign = int(request.form.get('Assign'))
+       else:
+         Assign = None
+
+       if sheet.cell(row=1,column=1).value is None and sheet.cell(row=1,column=2).value is None and sheet.cell(row=1,column=3).value is None and sheet.cell(row=1,column=4).value is None:
+          sheet.cell(row=1,column=1,value='Reg_no')
+          sheet.cell(row=1, column=2, value='IA_one')
+          sheet.cell(row=1, column=3, value='IA_two')
+          sheet.cell(row=1, column=4, value='Assig')
+
+       for row in range(2, sheet.max_row + 1):
+            if sheet.cell(row=row, column=1).value == Reg_no:
+                sheet.cell(row=row, column=2).value = IA_one
+                sheet.cell(row=row, column=3).value = IA_two
+                sheet.cell(row=row, column=4).value = Assign
+                break
+            elif sheet.cell(row=row,column=2).value is None:
+               sheet.cell(row=row,column=2).value = IA_one
+            elif sheet.cell(row=row,column=3).value is None:
+               sheet.cell(row=row,colummn=3).value = IA_two
+            elif sheet.cell(row=row,column=4).value is None:
+               sheet.cell(row=row,column=4).value = Assign
+       else:
+          data = [Reg_no,IA_one,IA_two,Assign]
+          sheet.append(data)
+       wb.save('Marks.xlsx')
+       row +=1
+    return render_template("home.html",Reg_no=Reg_no,IA_one=IA_one,IA_two=IA_two,Assign=Assign)
 
 if __name__=="__main__":
     app.run(debug=True)
